88ce56f3d4619947cccf887fc95f54352892807a
Polina Todorova <p.v.todorova@abv.bg>
2018-06-24 20:32:46 +0300

    Add some functionality files

---
+++ controllers/showOwnFiles.php
@@ -0,0 +1,26 @@
+<?php
+require_once "../libs/Init.php";
+Init::_init(true);
+
+use libs\DocumentInfo;
+
+function showOwnFiles()
+{
+	$_SESSION['user'] = 'polly';
+
+	$username = $_SESSION['user'];
+
+	$sql= "SELECT * FROM documents WHERE owner = ?";
+	$stmt = (new Db())->getConn()->prepare($sql);
+	$result = $stmt->execute([$username]);
+
+	$documentArray = array();
+
+	while($row = $result->fetch_assoc()){
+		$document = new DocumentInfo(row['owner'], row['contentUrl'], row['lastUpdateUsername'], row['lastUpdateDate']);
+		$documentArray[] = $document;
+	}
+
+	return $documentArray;
+}
+?>
---
+++ controllers/viewDocument.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../libs/Init.php";
+Init::_init(true);
+
+use libs\DocumentInfo;
+
+function viewDocument($fileName)
+{
+    $_SESSION['user'] = 'polly';
+
+    $username = $_SESSION['user'];
+    $path = $username . "/" . $fileName;
+
+    $sql = "SELECT * FROM documents WHERE cotentUrl = ?";
+    $stmt = (new Db())->getConn()->prepare($sql);
+    $result = $stmt->execute([$path]);
+
+    $row = $result->fetch_assoc();
+
+    $document = new DocumentInfo(row['owner'], row['contentUrl'], row['lastUpdateUsername'], row['lastUpdateDate']);
+
+
+    $filePath = "users/" . $path;
+    $fileContent = file_get_contents("$filePath");
+    echo $fileContent;
+
+    return $document;
+}
+?>
